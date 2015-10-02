9fd0174d9458595f53eb862a26a6a776002bbc74
Marcin Kruglik <marcin.kruglik@gmail.com>
2015-10-02 20:55:52 +0100

    ADD DATE OF BIRTH FIELD WHICH WOULD REPLACE AGE

--- src/com/myFirstApp/Person.java
+++ src/com/myFirstApp/Person.java
@@ -1,9 +1,12 @@
 package com.myFirstApp;
 
+import java.time.LocalDate;
+
 public class Person {
 	private String name;
-	private int age;
+	private int age = 0;
 	private String username;
+	private LocalDate dob;
 	
 	public Person() {
 		
@@ -12,6 +15,12 @@ public class Person {
 	public Person(String name, int age, String username) {
 		this.name = name;
 		this.age = age;
+		this.username = username;
+	}
+	
+	public Person(String name, LocalDate dob, String username) {
+		this.name = name;
+		this.dob = dob;
 		this.username = username;
 	}
 
@@ -24,9 +33,12 @@ public class Person {
 	}
 
 	public int getAge() {
+	if (age == 0) {
+		return 0; // TODO calculate age from dob
+	} else
 		return age;
 	}
-
+	
 	public void setAge(int age) {
 		this.age = age;
 	}
@@ -38,6 +50,5 @@ public class Person {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
 	
 }
